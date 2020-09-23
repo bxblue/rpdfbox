57f827a6bab61a70ad0eb9073febb0291abe7bd8
Marina Joranhezon <marina.joranhezon@gmail.com>
2020-09-23 09:05:26 -0300

    fix: downgrade pdf jar

--- lib/rpdfbox.rb
+++ lib/rpdfbox.rb
@@ -2,5 +2,5 @@ require 'fileutils'
 Dir[File.expand_path(File.dirname(__FILE__)) + "/rpdfbox/*.rb"].each{|f| require f}
 # Location of our PDF Box executable jar
 module RPDFBox
-  $PDFBOX_EXECUTABLE = File.expand_path(File.dirname(__FILE__)) + "/jars/pdfbox-app-2.0.21.jar"
+  $PDFBOX_EXECUTABLE = File.expand_path(File.dirname(__FILE__)) + "/jars/pdfbox-app-1.8.16.jar"
 end
